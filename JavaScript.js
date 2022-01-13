5d63e5889cf093427f491ea35061d6fc54b3fd6e
PinkBeeMe <56292374+pink-bee-me@users.noreply.github.com>
2022-01-13 09:26:39 -0600

    Upload of current portfolio page for creatingthecode.com

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+
+  
+  // These functions open and close the contact form 
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  
+  }
+
+  function buttonPink() {
+    document.getElementById("myForm").style.display="block";
+  }
